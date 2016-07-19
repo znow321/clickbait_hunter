66cf3137f4d94069e91be8ea630d7d9783b2d7cb
David <davidos313@gmail.com>
2016-07-19 22:47:48 +0200

    Dynamic obsolete content deletion now working... Have to refactor because the code is a mess...

--- clickbait_identifier_algorithm.py
+++ clickbait_identifier_algorithm.py
@@ -34,13 +34,11 @@ def error(message, sleep_time=0):
 class DatabaseTools:
 	#  Words - All together, Sentences - All together, Statistics - clickbait only
 	database = {'words':{},'sentences':{},
-				'statistics':{'avg_lower':0, 'avg_start_upper':0, 
-				'avg_upper':0, 'avg_num':0}}
-
+		    'statistics':{'avg_lower':0, 'avg_start_upper':0, 
+		    'avg_upper':0, 'avg_num':0}}
 
 	def __database_exists(self):
 		return glob(self.save_dir)
-
 
 	def db_connect(function):
 		def wrapper(self):
@@ -68,8 +66,7 @@ class DatabaseTools:
 			self.db_cursor.execute("SELECT keyword, value FROM Statistics")
 			for key, value in self.db_cursor.fetchall():
 				DatabaseTools.database['statistics'][key] = value
-		self.db_copy = DatabaseTools.database  #  To compare what words to delete
-
+		self.db_legacy = DatabaseTools.database  #  To compare what words to delete
 
 	#  Direct insertion of new 'profiles', no further steps needed
 	def sql_insert(self, table, val1, val2):
@@ -83,56 +80,54 @@ class DatabaseTools:
 	def sql_update(self, table, column_to_update, comparison_column, new_value, current_row):
 		template = "UPDATE {TABLE} SET {VALUE} = {NEW_VALUE} WHERE {KEY} ="\
 				   "{CURRENT_ROW}".format(TALBE=table, VALUE=column_to_update,
-				   								NEW_VALUE=new_value, KEY=comparison_column,
-				   								CURRENT_ROW=current_row)
+				   			  NEW_VALUE=new_value, KEY=comparison_column,
+				  			  CURRENT_ROW=current_row)
 		input(command)
 		self.db_cursor.execute(command)
 		self.db_conn.commit()
 
 
-	def sql_update_or_input(self, tables_info):  #  Ugly method, needs refactoring...
-		self.remove_queue()  # DEBUG
-		for table in tables_info:
-			for current_row_name, new_value in DatabaseTools.database[table[0].lower()].items():
-				if self.item_exists(table[0], table[1], current_row_name):
-					if not self.is_value_current(table[0], table[2], table[1], new_value, current_row_name):
-						self.sql_update(table[0], table[2], table[1], new_value, current_row_name)  #  Update
-				else:
-					self.sql_insert(table[0], current_row_name, new_value)  #  Insert
+	def update_all(self, tables_info):  #  Ugly method, needs refactoring...
+		self.remove_queue(tables_info)
+		for table, values in tables_info.items():
+			column = values[0]
+			update_column = values[1]
 
+			for current_row_name, new_value in DatabaseTools.database[table.lower()].items():
+				if self.item_exists(table, column, current_row_name):
+					if not self.is_value_current(table, update_column, column, new_value, current_row_name):
+						self.sql_update(table, update_column, column, new_value, current_row_name)  #  Update
+				else:
+					self.sql_insert(table, current_row_name, new_value)  #  Insert
 
 	def item_exists(self, table, column, column_value):  #  Works fine
-		template = 'SELECT {COLUMN} FROM {TABLE} WHERE {COLUMN} = ?'
-		command = sub('{COLUMN}', column, template)
-		command = sub('{TABLE}', table, command)
+		command = 'SELECT {COLUMN} FROM'\
+		' {TABLE} WHERE {COLUMN} = ?'.format(COLUMN=column,
+											 TABLE=table)
 		self.db_cursor.execute(command, (column_value,))
 		return len(self.db_cursor.fetchall()) > 0
 
+	def remove_queue(self, tables_info):  #  Have to config this not sure what is wrong here....
+		for table, values in self.db_legacy.items():
+			for item_name, value in values.items():
+				if not item_name in DatabaseTools.database[table]:
+					self.item_remove(table, tables_info[table.title()][0], item_name)
 
-	def remove_queue(self):  #  Have to config this not sure what is wrong here.... 
-		print(DatabaseTools.database)
-		print(self.db_copy)
-#		for current, legacy in zip(DatabaseTools.database, self.db_copy):
-#			print("current value: %s" % (str(current)))
-#			input("Legacy value: %s" % (str(legacy)))
+	def item_remove(self, table, column, item_name):  #  Remove non-existent values
+		command = 'DELETE FROM {TABLE} WHERE' \
+		' {COLUMN} = "{ITEM_NAME}"'.format(TABLE=table, 
+										 COLUMN=column,
+										 ITEM_NAME=item_name)
 
+		self.db_cursor.execute(command)
+		self.db_conn.commit()
 
-	def item_remove_command(self, table, column):  #  Remove non-existent values
-		template = 'DELETE FROM {TABLE} WHERE {COLUMN} = ?'
-		command = sub('{TABLE}', table, template)
-
-		return sub('{COLUMN}', column, command)
-
-
-	def is_value_current(self, table, column_to_update, 
-						 comparison_column, new_value, 
-						 current_row_name):
-
+	def is_value_current(self, table, column_to_update, comparison_column, new_value, current_row_name):
 		command = 'SELECT {COMPARISON_COLUMN} FROM {TABLE} '\
 			  'WHERE {COLUMN_TO_UPDATE} = {NEW_VALUE} AND'\
 			  ' {COMPARISON_COLUMN} = "{CURRENT_ROW_NAME}"'.format(COMPARISON_COLUMN=comparison_column,
-				   													  TABLE=table, COLUMN_TO_UPDATE=column_to_update,
-				   													  NEW_VALUE=new_value, CURRENT_ROW_NAME=current_row_name)
+				   					       TABLE=table, COLUMN_TO_UPDATE=column_to_update,
+				 					       NEW_VALUE=new_value, CURRENT_ROW_NAME=current_row_name)
 		self.db_cursor.execute(command)
 		return len(self.db_cursor.fetchall()) > 0
 
@@ -141,19 +136,20 @@ class DatabaseTools:
 			self.db_cursor.execute("CREATE TABLE IF NOT EXISTS %s" % (table))
 
 	def save_database(self):
+		del DatabaseTools.database['statistics']['avg_upper']
 		self.db_conn = sqlite3.connect(self.save_dir)
 		self.db_cursor = self.db_conn.cursor()
 		#  ( Table_name, comparison_column, new_value )
-		tables_info = (('Words', 'word', 'clickbait_index'),
-				       ('Sentences', 'sentence', 'clickbait_status'),
-				       ('Statistics', 'keyword', 'value'))
+
+		tables_info = {'Words':('word', 'clickbait_index'),
+				       'Sentences':('sentence', 'clickbait_status'),
+				       'Statistics':('keyword', 'value')}
 
 		#  Table creation template
-		tables = list(['%s(%s TEXT, %s INTEGER)' % (table, key, value) for table, key, value in tables_info])
+		tables = list(['%s(%s TEXT, %s INTEGER)' % (key, value[0], value[1]) for key, value in tables_info.items()])
 
 		self.sql_create_tables(tables)
-		self.sql_update_or_input(tables_info)
-		self.db_conn.commit()
+		self.update_all(tables_info)
 
 
 class WordTools:
