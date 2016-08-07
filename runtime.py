6026b59adc149d2a4017880264ae9572d7f8408c
David <davidos313@gmail.com>
2016-08-07 22:07:02 +0200

    I could potentially drop some tables and calculate needed values from the sentence database.

--- runtime.py
+++ runtime.py
@@ -3,6 +3,8 @@ from utils import init_save_dir
 from database import database
 from analysis import analyze
 from database_tools import db_save, db_load
+from re import sub
+
 
 # Most of other modules won't work without the database class
 # It looks like the static database class method variables are shared between classes regardless where they are imported.
@@ -12,16 +14,16 @@ init_save_dir()
 
 
 def start_identifier(sentence): # THIS IS THE ABSOLUTELY FIRST FUNCTION TO CALL 
-    database.sentence = sentence
+    database.sentence = sub(r'[^A-Za-z0-9]', sentence)
     route()
 
 # DEBUG
 
 db_load()
-database.sentence = "This is a very important test."
+database.sentence = "This is a very important test"
 database.clb_status = True
 analyze()
 input(database.__dict__)
-db_save()
+# db_save()
 
 # DEBUG
--- statistics.py
+++ statistics.py
@@ -7,16 +7,9 @@ from database import database
 def report_gen():
     # [ lowercase, upper_start, upper, number]  
     sentence = database.sentence
-    yield findall(r'\b[a-z\']+\b', sentence)
-    yield findall(r'\b[A-Z][a-z\']+\b', sentence)
-    yield findall(r'\b[A-Z\']+\b', sentence)
-    yield findall(r'\b[0-9]+\b', sentence)
-
-
-def report_len():
-    stats = list(report_gen())
-    input(stats)
-    return [len(word_type) for word_type in stats]
+    templates = (r'\b[a-z\']+\b',r'\b[A-Z][a-z\']+\b',r'\b[A-Z\']+\b',r'\b[0-9]+\b')
+    for template in templates:
+        yield len(findall(template, sentence))
 
 
 def percentages(sentence = database.sentence): # For both global & current 
@@ -27,7 +20,7 @@ def percentages(sentence = database.sentence): # For both global & current
 
 
 def update_statistics_db():
-    wordcount = report_len() 
+    wordcount = list(report_gen()) 
     database.database['statistics']['num_lower'] = wordcount[0]
     database.database['statistics']['num_upper_start'] = wordcount[1]
     database.database['statistics']['num_upper'] = wordcount[2]
