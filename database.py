3126aef25b4929bd8126b7f001876067f757cd61
David <davidos313@gmail.com>
2016-08-08 11:27:42 +0200

    Adding avg calculation methods.

--- database.py
+++ database.py
@@ -4,9 +4,6 @@ class database: # Universal data storage function
     tables = ('words', 'sentences')
 
     database = {'words':{},'sentences':{},
-                'statistics':{'num_lower':0, # Total number of analyzed specimens needed for statistical analysis
-                'num_upper_start':0,
-                'num_upper':0,
-                'num_int':0}}
+                }
 
     db_legacy = {}  #  Original copy for db item removal
--- statistics.py
+++ statistics.py
@@ -4,7 +4,7 @@ from re import findall
 from database import database
 
 
-# Only for temporary calculations
+# [lower, upper_start, upper, num]
 
 
 def lower(sentence = database.sentence):
@@ -23,13 +23,6 @@ def number(sentence = database.sentence):
     return len(findall(r'\b[0-9]+\b',sentence))
 
 
-def update_statistics_db():
-    database.database['statistics']['num_lower'] = lower()
-    database.database['statistics']['num_upper_start'] = upper_start()
-    database.database['statistics']['num_upper'] = upper()
-    database.database['statistics']['num_int'] = number()
-
-
 def percentages(sentence = database.sentence): # For both global & current 
     report = report_len()
     percent_per_word = 100 / report[0]
@@ -37,18 +30,33 @@ def percentages(sentence = database.sentence): # For both global & current
         yield word_type * percent_per_word
 
 
+def cur_ratio():
+    cur_ratio = [ 0, 0, 0, 0 ]
+    cur_ratio[0] = lower(sentence)
+    cur_ratio[1] = upper_start(sentence)
+    cur_ratio[2] = upper(sentence)
+    cur_ratio[3] = number(sentence)
+    return cur_ratio
+
+
 def global_avg_ratio():
     global_ratio = [ 0, 0, 0, 0 ]
     for sentence, clb_status in database.database['sentences'].items():
-       global_stats[0] += lower(sentence)
-       global_stats[1] += upper_start(sentence)
-       global_stats[2] += upper(sentence)
-       global_stats[3] += number(sentence)
+       global_ratio[0] += lower(sentence)
+       global_ratio[1] += upper_start(sentence)
+       global_ratio[2] += upper(sentence)
+       global_ratio[3] += number(sentence)
     return global_ratio
 
 
+def cur_weight
+
+
 def global_avg_weight():
-    global_weight = 0
+    weight_list = [] # For final calculation
     for sentence, clb_status in database.database['sentences'].items():
+        cur_weight = 0
         for word in sentence.split():
-            pass
+           cur_weight += database.database['words'][word] 
+        weight_list.append(cur_weight)
+    return sum(weight_list) / len(weight_list)
